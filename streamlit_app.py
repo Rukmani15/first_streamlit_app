0cdc63bd476845d721225929a8e0e9678ea21908
Rukmani15 <122947189+Rukmani15@users.noreply.github.com>
2023-10-26 19:54:05 -0400

    Update streamlit_app.py
    Added Emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit
 
 streamlit.title('My Parents new Healthy Diner');
 streamlit.header('Breakfast Menu');
-streamlit.text('Omega 3 & Blueberry Oatmeal');
-streamlit.text('Kale & Spinach Smoothie');
+streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal');
+streamlit.text(' 🥗Kale & Spinach Smoothie');
+streamlit.text(' 🐔Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞Avacado Toast');
